dd602f113b2a7c90bfb23ef5a7cb4ea8fa2c0324
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-10-12 23:15:37 +0530

    Day 2 : Roman To Integer

--- Roman2Int.py
+++ Roman2Int.py
@@ -10,7 +10,6 @@ Roman numerals are usually written largest to smallest from left to right
 """
 
 
-
 class Solution(object):
     def romanToInt(self, s):
         """
@@ -19,25 +18,23 @@ class Solution(object):
         """
         # Dictionary Containing the value map of roman to integer
         val = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        special = {"IX":9,"IV":4,"XL":40,"XC":90,"CD":400,"CM":900}
-        
-        #Length of the string and the Integer value 
-        n = len(s)
         sol = 0
+        for i in range(len(s)):
+            if i<len(s)-1 and val[s[i]] < val[s[i+1]]:
+                sol -=val[s[i]]
+            else:
+                sol += val[s[i]]
+        
+        
+        
+        return sol
 
-        #Loop for the solution 
-        if n%2!=0: 
-            for i in range(0,n,2):
-                if i!=n-1:
-                    j = i+1
-                    
-                print(val[s[i]],val[s[j]])
-
+## 
 
 test = Solution()
+s = input("Enter The Roman Number ")
+answer = test.romanToInt(s)
 
-s = input("ENTER THE TEST STRING")
-
-test.romanToInt(s)
+print(f"Interger Value of the Roman Number is {answer}")
 
 
