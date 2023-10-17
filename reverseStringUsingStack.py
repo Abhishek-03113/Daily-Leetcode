5f98b8dca65c744cfa31c8df71bd5cfb44988973
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-10-17 18:57:49 +0530

    Day 7 problem 3 reverse string

---
+++ reverseStringUsingStack.py
@@ -0,0 +1,19 @@
+"""
+@Abhishek 
+
+Day 7 Problem 3 
+
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        # s[:] = s[::-1]
+
+        # return s
+        return s.reverse()
+
+        
