5c78fd42dd037b70a168674ea09c0a63028087d1
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-10-22 00:11:38 +0530

    Day 12

---
+++ GrumpyBookStore.py
@@ -0,0 +1,22 @@
+"""
+@Abhishek 
+Day 12 
+Topic : strings 
+"""
+
+
+class Solution:
+    def maxSatisfied(
+        self, customers: List[int], grumpy: List[int], minutes: int
+    ) -> int:
+        k = minutes
+        n = len(customers)
+        res = sum([customers[i] * (1 - grumpy[i]) for i in range(n)])
+        best_gain = sum([customers[i] * grumpy[i] for i in range(k)])
+        gain = best_gain
+        for i in range(k, n):
+            gain += (
+                customers[i] * grumpy[i] - customers[i - k] * grumpy[i - k]
+            )  # add new, kick out old
+            best_gain = max(gain, best_gain)
+        return res + best_gain
