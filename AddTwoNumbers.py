4368bb93acbf898830159b82a0df99d0ec2d4c7f
= <=>
2023-11-01 19:03:17 +0530

    day 20 optimal

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -32,8 +32,8 @@ class Solution:
             tail.next = newNode
             tail = tail.next
 
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
         result = dummyHead.next
         dummyHead.next = None
