2c80d93066aeaeeb56b704ccf9ee94857f954801
abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-11-18 11:01:20 +0530

    Day 37 Daily Challenge

---
+++ maxFrequency.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        # nums.sort()
+
+        # left = 0
+        # ans = 0
+        # curr = 0
+
+        # for right in range(len(nums)):
+
+        #     target = nums[right]
+
+        #     curr += target
+
+        #     while (right - left + 1) * target-curr > k:
+
+        #         curr -= nums[left]
+
+        #         left += 1
+
+        #     ans = max(ans,right - left + 1)
+
+        # return ans
+        nums.sort()
+        left = 0
+        curr = 0
+
+        for right in range(len(nums)):
+            target = nums[right]
+            curr += target
+
+            if (right - left + 1) * target - curr > k:
+                curr -= nums[left]
+                left += 1
+
+        return len(nums) - left
