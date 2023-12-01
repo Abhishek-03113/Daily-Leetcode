bf560d5b15419cba6fc23e6fd6785a26d4a0f3f9
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-12-01 19:28:17 +0530

    Doubly Linked List

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -4,3 +4,36 @@ class Node:
         self.prev = None
         self.next = None 
 
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.head = None
+    
+    def append(self,data):
+        if self.head is None: 
+            newNode = Node(data)
+            self.head = newNode
+        else:
+            newNode = Node(data) 
+            curr = self.head 
+
+            while curr.next:
+                curr = curr.next 
+            curr.next = newNode 
+            newNode.prev = curr 
+
+    def prepend(self,data):
+        if self.head is None:
+            newNode = Node(data) 
+            self.head = newNode 
+        else:
+            
+            ## prepend means before the head node so  
+            newNode = Node(data)
+            
+            
+
+    def print_list(self):
+        pass
+
