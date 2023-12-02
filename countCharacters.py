3ee7d3345b5181efc24861fa8adab7b8f206fde4
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-12-02 09:44:02 +0530

    day 49

--- countCharacters.py
+++ countCharacters.py
@@ -1,26 +1,46 @@
 class Solution:
     def countCharacters(self, words: List[str], chars: str) -> int:
-        count = defaultdict(int) 
+        # count = defaultdict(int) 
 
-        for c in chars:
-            count[c] += 1
+        # for c in chars:
+        #     count[c] += 1
         
-        ans = 0 
+        # ans = 0 
 
-        for word in words: 
-            word_count = defaultdict(int) 
+        # for word in words: 
+        #     word_count = defaultdict(int) 
 
-            for c in word: 
-                word_count[c] += 1 
+        #     for c in word: 
+        #         word_count[c] += 1 
+            
+        #     good = True
+
+        #     for c, freq in word_count.items(): 
+        #         if count[c] < freq: 
+        #             good = False
+        #             break
+            
+        #     if good: 
+        #         ans += len(word)
+        
+        # return ans
+        counts = [0] * 26
+        for c in chars:
+            counts[ord(c) - ord("a")] += 1
+        
+        ans = 0
+        for word in words:
+            word_count = [0] * 26
+            for c in word:
+                word_count[ord(c) - ord("a")] += 1
             
             good = True
-
-            for c, freq in word_count.items(): 
-                if count[c] < freq: 
+            for i in range(26):
+                if counts[i] < word_count[i]:
                     good = False
                     break
             
-            if good: 
+            if good:
                 ans += len(word)
-        
+            
         return ans
