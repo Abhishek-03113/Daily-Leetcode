77f626b515c0d4283ee49470917cbf0272d43887
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-12-16 10:29:39 +0530

    Day 65

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -32,3 +32,28 @@ class Solution:
         # Sorting Solution
 
     # return sorted(s) == sorted(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        m = len(s)
+        n = len(t)
+        if m != n:
+            return False
+        
+        m_len = defaultdict(int)
+        n_len = defaultdict(int)
+
+
+        for i in range(m):
+
+            m_len[s[i]] = 1+ m_len.get(s[i],0)
+            
+            n_len[t[i]] = 1+ n_len.get(t[i],0)
+        
+        
+        for i in m_len:
+
+            if m_len[i] != n_len[i]:
+                return False
+
+        return True 
