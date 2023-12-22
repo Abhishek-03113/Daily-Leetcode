787f61840d954c6d07a0270a3889491dee3e8bee
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2023-12-22 12:23:37 +0530

    Leetcode DCC 21-Dec-23

---
+++ maxWidthVerticalArea.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
+        x_sorted = sorted([x for x, _ in points])
+
+        max_width = 0
+        for i in range(len(x_sorted) - 1):
+            max_width = max(max_width, x_sorted[i + 1] - x_sorted[i])
+
+        return max_width        
+        
