c1f0969393bd0c4cb6c812c23fc508284095bd19
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-01-15 21:22:12 +0530

    POTD

--- max_Courses.py
+++ max_Courses.py
@@ -19,3 +19,14 @@ class Solution:
         return f(0, total, cost, dp)
             
         
+import math
+from typing import List
+
+
+class Solution:
+    def max_courses(self, n : int, total : int, cost : List[int]) -> int:
+        dp = [0 for _ in range(total + 1)]
+        for c in reversed(cost):
+            for t in range(total, c-1, -1):
+                dp[t] = max(dp[t], dp[t - (c + 9)//10] + 1)
+        return dp[total]
