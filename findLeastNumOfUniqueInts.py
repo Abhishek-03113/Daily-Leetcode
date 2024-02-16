21a0504dc8b7102f2f9f8ee4347e3e3cd4281c25
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-02-16 21:53:42 +0530

    DCC feb 16

---
+++ findLeastNumOfUniqueInts.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
+
+        mp = Counter(arr)
+
+
+
+        v = list(mp.values()) 
+
+
+
+        cnt = 0 
+
+        v.sort() 
+
+
+
+        for i in range(len(v)):
+
+
+
+            if k > v[i]:
+
+                k -= v[i]
+
+                v[i] = 0 
+
+            else:
+
+                v[i] -= k 
+
+                k = 0 
+
+            if v[i] != 0:
+
+                cnt += 1
+
+            
+
+        return cnt
