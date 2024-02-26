28f7d3ce4ef652f25aba4e8c8b55bbe629aa567c
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-02-26 21:38:04 +0530

    DCC

---
+++ code/python/cardGame.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+for _ in range(t):
+    # print("HELLO")
+    n = int(input())
+
+    trump = input()
+
+    a = list(input().split()) 
+    
+    a = sorted(a)
+
+    flag = False 
+    
+    pairs = []
+    
+    
