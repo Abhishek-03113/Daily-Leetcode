4559ff71f5b27deb85cd404bd40fbf97f97848e8
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-02-27 22:07:58 +0530

    practice

--- code/python/cardGame.py
+++ code/python/cardGame.py
@@ -2,15 +2,32 @@ from collections import defaultdict
 t = int(input())
 
 for _ in range(t):
-    # print("HELLO")
     n = int(input())
 
     trump = input()
 
     a = list(input().split()) 
     
-    
-    
+    h = {
+        'S': [],
+        'H': [],
+        'D': [],
+        'C': []
+    }
 
+    for i in a:
+        h[i[1]].append(i[0])
     
+    
+    for i in h:
+    
+        if len(h[i]) == 0:
+            continue 
+            
+        if len(h[i]) % 2 == 0:
+            for j in range(len(h[i])):
+                temp = h[i].pop(j)
+                print(f"{i}{temp}", end=" ")
+                
+            print("")
     
