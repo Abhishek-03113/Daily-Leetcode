0c648e9eb7136f62027444c406e72bb1051aa94f
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-03-10 16:35:17 +0530

    POTD

---
+++ removeDuplicatesFromString.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+
+	
+
+	def removeDuplicates(self,str):
+
+	    
+
+	    ans = ""
+
+	    
+
+	    for i in str: 
+
+	        if i not in ans: 
+
+	            ans += i
+
+        return ans
+
+	    # code here
+
+
