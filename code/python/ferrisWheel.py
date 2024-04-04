90caafae7f05e68a6a49693b626267a8ac4ed791
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-04-04 13:23:25 +0530

    cses

--- code/python/ferrisWheel.py
+++ code/python/ferrisWheel.py
@@ -1,27 +1,30 @@
-n, x = map(int, input().split()) 
+n, x = map(int, input().split())
 
-ans = 0  
+ans = 0
 
-a = list(map(int, input().split())) 
+a = list(map(int, input().split()))
 
 a.sort()
+
+# i = 1
+# while i < n:
+#     if a[i] + a[i - 1] <= x:
+#         i += 2
+#         ans += 1
+#     else:
+#         ans += 2
+#         i += 1
+# print(ans)
 
 pt1 = 0
 pt2 = n - 1
 
-while(pt1 < pt2):
+while pt1 < pt2:
     if a[pt1] + a[pt2] <= x:
         a[pt2] += a[pt1]
         pt1 += 1
-        
+        pt2 -= 1
     else:
         pt2 -= 1
-        
+
 print(n - pt1)
-    
-    
-    
-
-
-
-
