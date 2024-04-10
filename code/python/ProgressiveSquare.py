6bd6da1222bf6c50ebb3ce709392fbae4e30fd0d
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-04-10 12:13:07 +0530

    cf div 3 b

--- code/python/ProgressiveSquare.py
+++ code/python/ProgressiveSquare.py
@@ -4,8 +4,12 @@ for _ in range(int(input())):
     
     a = list(map(int, input().split())) 
     
+    
     a.sort()  
+    
     b = [0] * (n * n) 
+    
+    b[0] = a[0]
     
     for i in range(1, n): 
         b[i] = b[i-1] + c 
@@ -14,7 +18,7 @@ for _ in range(int(input())):
     for i in range(1, n): 
         for j in range(n): 
             b[i * n + j] = b[(i-1) * n + j] + d
+            
+    b.sort()
     
-    
-    print(a) 
-    print(b) 
+    print("YES" if a == b else "NO")
