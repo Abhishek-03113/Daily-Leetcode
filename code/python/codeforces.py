17ff7f43145cd86122007177e098828bb547c5a4
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-02 19:15:20 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,32 +1,70 @@
+def solve():
+    # n = int(input()) 
+    # a = list(map(int, input().split())) 
+    # a.sort() 
+    
+    # if n % 2:
+    #     return ["NO" ]
+    
+    # ans = [0]*n 
+    
+    # j = 0
+    # k = n // 2
+    
+    # for i in range(n):
+        
+    #     if i % 2 == 0:
+    #         ans[i] = a[j]
+    #         j += 1
+            
+    #     else:
+    #         ans[i] = a[k]
+    #         k +=1     
+
+    
+    # for i in range(1, n-1): 
+        
+        
+    #     if ans[i]>ans[i-1] and ans[i] > ans[i+1]:
+    #         continue
+            
+    #     elif ans[i]< ans[i-1] and ans[i] < ans[i+1]:
+    #         continue 
+            
+    #     else:
+    #         return ["NO"]
+    # print("YES")
+    # return ans
 for _ in range(int(input())):
+    
+    ans = solve() 
+    print(*ans)
+    # n = int(input())
+    # s = list(input())
 
+    # open = 0
 
-    n = int(input())
-    s = list(input())
+    # for i in range(n):
 
-    open = 0
+    #     if s[i] == '(':
+    #         open += 1
 
-    for i in range(n):
+    #     if s[i] == '_' and open > 0:
+    #         s[i] = ')'
+    #         open -= 1
+    #     elif s[i] == '_' and open == 0:
+    #         s[i] = '('
+    #         open += 1
+    #     elif s[i] == ")":
+    #         open-=1
 
-        if s[i] == '(':
-            open += 1
+    # stack = []
 
-        if s[i] == '_' and open > 0:
-            s[i] = ')'
-            open -= 1
-        elif s[i] == '_' and open == 0:
-            s[i] = '('
-            open += 1
-        elif s[i] == ")":
-            open-=1
+    # ans = 0
 
-    stack = []
-
-    ans = 0
-
-    for i in range(n):
-        if s[i] == '(':
-            stack.append(i)
-        else:
-            ans += (i-stack.pop())
-    print(ans)
+    # for i in range(n):
+    #     if s[i] == '(':
+    #         stack.append(i)
+    #     else:
+    #         ans += (i-stack.pop())
+    # print(ans)
