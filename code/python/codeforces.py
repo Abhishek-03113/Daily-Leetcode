b0a1233621a67987c5d3cfe57e38514672473638
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-06 19:58:30 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,56 +1,102 @@
-def help(s,t, x): 
+def help(): 
     pass
+
 import math
+
 def solve():
     
-    n = int(input()) 
-    p = list(map(int, input().split()))
+    n = int(input())
+    a = list(map(int, input().split()))
     
-    ans = 0 
     
-
-    mid = n // 2
-    sort = True 
-    rev = True
+    mid = n // 2 
     
-    for i in range(n):
-        if p[i] != n - i:
-            rev = False
-            break
+    l = 0
+    r = 0
     
-    for i in range(n):
-        if p[i] != i + 1:
-            sort = False
-            break
+    
+    for i in range(0, mid):
         
-    if sort:
+        if a[i] > a[mid]:
+            l += 1
+            break
+        else:
+            continue 
+    
+    for i in range(mid+1, n-1):
+        if a[i] < a[mid]:
+            r += 1 
+            break
+        else:
+            continue 
+    
+    
+    sorted_ = 1         
+    
+    for i in range(1,n):
+        if a[i] < a[i - 1]:
+            sorted_ = 0 
+            break 
+    
+    # print(a)
+    if sorted_:
         print(0)
-    elif rev:
-        print(3)
-    elif p[mid] == mid + 1:
+        return
+
+    if l == 0 and r == 0:
         print(1)
-    else:
+        return
+    elif l and r:
+        print(3)
+        return
+    elif l or r:
         print(2)
-        
-
+        return  
     
         
-    # n = int(input())
-    # s = input()     s
-    # win = n
-
-    # optimal = {'R':'P', 'P':'S', 'S':'R'}
-    # prev = ''
+        
     
-    # for i in range(n):    
-    #     if prev == optimal[s[i]]:
-    #         win -= 1
-    #         prev = ''
+    # odd = sum(1 for x in a if x % 2 != 0)
+    # even = n - odd     
+    # if even == n or odd == n:
+    #     print(0)
+    #     return 
+    
+    # # a.sort() 
+    
+    # max_even = 0 
+    # max_odd = 0
+    # ans  = 0
+    
+    # v = []
+    
+    # for i in range(n):
+    #     if a[i] % 2 == 0:
+    #         v.append(a[i])
     #     else:
-    #         prev = optimal[s[i]]
-            
-    # print(win)
+    #         max_odd = max(a[i], max_odd)
+        
+    # ans = even 
+    
+    # v.sort() 
+    
+    # for i in v:        
+    #     if i < max_odd:
+    #         max_odd += i 
+    #     else:
+    #         ans += 1
+    #         break
+    
+    # print(ans)
 
+
+                
+            
+        
+        
+    
+    
+    
 for _ in range(int(input())):
     solve()
 
