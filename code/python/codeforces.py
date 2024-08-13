c3cb631fd8b003736096e6b2475b060ac31e884f
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-13 21:56:49 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -5,38 +5,8 @@ def help():
     
 
 def solve():
-    # pass
-    n = int(input())
-    a = list(map(int, input().split())) 
-    s = list(input())
-   
-    # print(a,s)
     
-    score  = 0 
-    
-    l = 0 
-    r = n-1
-    
-    while l < r:
-        if s[l] == "L" and s[r] == "R":
-            
-            score += sum(a[l:r+1])
-            l += 1
-            r -= 1
-        
-        elif a[l] != "L":
-            l += 1 
-        elif a[r] != "R":
-            r -= 1 
-        
-    print(score)    
-    
-    
-        
-        
-        
-        
 
-    
+        
 for _ in range(int(input())):
     solve()
