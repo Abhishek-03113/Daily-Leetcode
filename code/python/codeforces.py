c5553b0a3bc45bd7b0e5f3b2711229836e0e087a
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-13 21:41:52 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,68 +1,42 @@
 import math 
 
-def help(num):
+def help():
     pass
     
+
 def solve():
+    # pass
+    n = int(input())
+    a = list(map(int, input().split())) 
+    s = list(input())
+   
+    # print(a,s)
     
-    n = int(input())    
-    a = list(map(int,input().split())) 
+    score  = 0 
     
-    m = int(input()) 
+    l = 0 
+    r = n-1
     
-    s = [] 
-    
-    for i in range(m):
-        s.append(input())
-    
-    # open = set()
-    
-    # for i, seat in enumerate(a):
-        
-    #     if i == 0:
-    #         open.add(seat)
-    #         continue 
+    while l < r:
+        if s[l] == "L" and s[r] == "R":
             
-    #     if not open:
-    #         print("NO")
-
-    #     if seat - 1 not in open and seat + 1 not in open and open:
-    #         print("NO")
-    #         return
-
-    #     open.add(seat) 
+            score += sum(a[l:r+1])
+            l += 1
+            r -= 1
         
+        elif a[l] != "L":
+            l += 1 
+        elif a[r] != "R":
+            r -= 1 
+        
+    print(score)    
     
-    # print("YES")
+    
+        
+        
+        
         
 
-
-    
-    
-    
-    # a = str(int(input())) 
-    
-    # if not a.startswith('10') or len(a) < 3:
-    #     print("NO")
-    #     return 
-
-    # exp = a[2:] 
-    
-    
-    # if exp.startswith("0"):
-    #     print("NO")
-    #     return
-    
-    # if exp.isdigit() and int(exp) >= 2:
-    #     print("YES")
-    #     return
-    # else:
-    #     print("NO")
-    #     return
-    
-    
-    
-    
     
 for _ in range(int(input())):
     solve()
