e7436e4a3a60e3c680fc466cbe7970a4788bd9b4
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-13 20:42:08 +0530

    codeforces today

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -4,14 +4,65 @@ def help(num):
     pass
     
 def solve():
-    x,y,k = map(int, input().split())
     
-    for i in range(0, k - k % 2):
-        print(x - ((1 if i % 2 != 0 else -1) * (i//2 + 1)), y) 
+    n = int(input())    
+    a = list(map(int,input().split())) 
+    
+    m = int(input()) 
+    
+    s = [] 
+    
+    for i in range(m):
+        s.append(input())
+    
+    # open = set()
+    
+    # for i, seat in enumerate(a):
         
-    if k % 2 != 0:
-        print(x, y)
+    #     if i == 0:
+    #         open.add(seat)
+    #         continue 
+            
+    #     if not open:
+    #         print("NO")
+
+    #     if seat - 1 not in open and seat + 1 not in open and open:
+    #         print("NO")
+    #         return
+
+    #     open.add(seat) 
         
+    
+    # print("YES")
+        
+
+
+    
+    
+    
+    # a = str(int(input())) 
+    
+    # if not a.startswith('10') or len(a) < 3:
+    #     print("NO")
+    #     return 
+
+    # exp = a[2:] 
+    
+    
+    # if exp.startswith("0"):
+    #     print("NO")
+    #     return
+    
+    # if exp.isdigit() and int(exp) >= 2:
+    #     print("YES")
+    #     return
+    # else:
+    #     print("NO")
+    #     return
+    
+    
+    
+    
     
 for _ in range(int(input())):
     solve()
