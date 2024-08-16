2c559a009b9739dc15391d180db0f088286e5650
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-16 10:09:07 +0530

    B

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,16 +1,40 @@
 def solve():
-    n = int(input()) 
-    a = list(map(int,input().split())) 
-
-    if n > 2:
-        print("NO")
-        return
-    else:
-        if (abs(a[0] - a[1]))> 1:
-            print("YES") 
-            return
     
-    print("NO")
+    a, b = map(int, input().split())
+    c, d = map(int, input().split()) 
+    
+    ans = 0 
+    
+    print(f"Alice {a, b}  bob {c , d}")    
+    if a == c:
+        
+        if b == d:
+            print(b-a)
+            return 
+
+        print(min(b,d) - a + 1) 
+        
+    elif a < c:
+        
+        if b < c:
+            print(c-b)
+            return
+        elif b == c: 
+            print(1) 
+        elif b > c:
+            if b == d:
+                print(b - c + 1)
+            
+            elif b > d: 
+                print(b - c) 
+            
+    elif a > c: 
+        
+        
+            
+    
+ 
+    
 
 for _ in range(int(input())):
     solve() 
