8fea66599e912ca4616d484804d751ceac817048
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-16 09:16:18 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,12 +1,16 @@
-import math 
-
-def help():
-    pass
-    
-
 def solve():
-    
+    n = int(input()) 
+    a = list(map(int,input().split())) 
 
-        
+    if n > 2:
+        print("NO")
+        return
+    else:
+        if (abs(a[0] - a[1]))> 1:
+            print("YES") 
+            return
+    
+    print("NO")
+
 for _ in range(int(input())):
-    solve()
+    solve() 
