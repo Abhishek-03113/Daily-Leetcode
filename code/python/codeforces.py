9a5b316283ac5430c36187c2b77d6eca05d86b45
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-08-16 10:18:08 +0530

    B done'

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,40 +1,48 @@
 def solve():
-    
-    a, b = map(int, input().split())
-    c, d = map(int, input().split()) 
-    
-    ans = 0 
-    
-    print(f"Alice {a, b}  bob {c , d}")    
-    if a == c:
-        
-        if b == d:
-            print(b-a)
-            return 
 
-        print(min(b,d) - a + 1) 
-        
-    elif a < c:
-        
-        if b < c:
-            print(c-b)
-            return
-        elif b == c: 
-            print(1) 
-        elif b > c:
-            if b == d:
-                print(b - c + 1)
-            
-            elif b > d: 
-                print(b - c) 
-            
-    elif a > c: 
-        
-        
-            
+    n,k = map(int, input().split()) 
+    a = list(map(int,input().split())) 
     
- 
-    
+    # a, b = map(int, input().split())
+    # c, d = map(int, input().split())
+
+    # ans = 0
+
+    # # print(f"Alice {a, b}  bob {c , d}")
+    # if a == c:
+
+    #     if b == d:
+    #         print(b - a)
+    #         return
+
+    #     print(min(b, d) - a + 1)
+
+    # elif a < c:
+
+    #     if b < c:
+    #         print(c - b)
+    #         return
+    #     elif b == c:
+    #         print(1)
+    #     elif b > c:
+    #         if b == d:
+    #             print(b - c + 1)
+
+    #         elif b > d:
+    #             print(b - c)
+
+    # elif a > c:
+
+    #     if d > b:
+    #         print(3)
+    #         return
+    #     elif d == b:
+    #         print(d - c + 1)
+    #         return
+    #     else:
+    #         print(d - c + 1)
+    #         return
+
 
 for _ in range(int(input())):
-    solve() 
+    solve()
