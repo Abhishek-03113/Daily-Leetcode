1ca8a2ec3e53966e28962801683acf77618df02c
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-10-30 19:05:59 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,14 +1,38 @@
-def solve():
-    y, x = map(int, input().split()) 
-    
-    n = max(y, x) * (max(y, x) - 1) + 1
-    
-    if max(x, y)%2 == 0:
-        n += y-x 
-    else:
-        n += x- y 
-    
-    print(n)
-     
+def upper_bound(a, x): 
+    l = 0 
+    r = len(a) 
+    while l < r: 
+        m = (l + r) // 2 
+        if a[m] <= x: 
+            l = m + 1 
+        else: 
+            r = m 
+            
+    if l - 1 < 0: 
+        return -1
+    return l   
 
-for _ in range(int(input())):solve()    
+
+def solve():
+    n, m = map(int, input().split()) 
+    
+    a = list(map(int, input().split())) 
+    b = list(map(int, input().split()))  
+    
+    a.sort()
+    
+    for i in range(m): 
+        ans = (upper_bound(a, b[i]))  
+        
+        if ans <= 0: 
+            print(-1) 
+        else: 
+            if ans > 0:
+                print(a[ans - 1])
+                a[ans - 1] = float('inf')
+            
+
+        
+solve() 
+
+# for _ in range(int(input())):solve()    
