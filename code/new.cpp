5d7c078719f1e7615dee85f6f85be469834ac4da
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-11-02 19:30:13 +0530

    coded some problems

--- code/new.cpp
+++ code/new.cpp
@@ -1,27 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 #define int long long
 #define nl '\n'
-// #define F first
-// #define S second
-// #define pb push_back
-// #define mp make_pair
-// #define V vector<int>
-// #define P pair<int, int>
+#define V vector<int>
+
 #define mod 1000000007
 #define inf 1e18
-#define print(x) for (int i= 0; i < x.size(); i++) cout << x[i] << " "; 
+#define print(x) for (int i = 0; i < x.size(); i++) cout << x[i] << " "; 
+#define loop(i, n) for (int i = 0; i < n; i++)
 
-void solve(){
-    
-}
-signed main(){
-    int t;
-    cin >> t;
-    while(t--){
-        solve();
+void solve(){ 
+   int n; cin >> n; 
+   V a(n+1, 0);
+
+   set<int> s; 
+
+   loop(i, n){ 
+    int x; cin >> x; a[i] = x; 
+   }
+
+   int curr, ans; 
+   
+   curr = 0; ans = 0;
+
+   loop(i, n){
+
+    if (s.find(a[i]) != s.end()){
+        ans = max(ans, curr); 
+        curr -= 1; 
+        s.erase(a[i]);
+        
     }
-    return 0;
+    else{
+        curr += 1; 
+        s.insert(a[i]);  
+    }
+    ans = max(ans, curr); 
+   }
+
+   cout << ans << nl; 
+   
+   
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();    
 }
