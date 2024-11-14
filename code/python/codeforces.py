0f3f4aefc9b45856e761c886897cd259a28d19c4
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-11-14 19:55:52 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,22 +1,52 @@
-def helper(ind, n, yn, memo):    
-    if ind in memo:
-        return memo[ind]
-    if ind == n:
-        return 0
-    if yn[ind] < 0:
-        memo[ind] = helper(ind + 1, n, yn, memo)
-    else:
-        memo[ind] = yn[ind]*yn[ind] + helper(ind + 1, n, yn, memo)
-    return memo[ind]
+import math
+from collections import Counter
 
+# def solve():
+    
+#     n, k = map(int, input().split()) 
+    
+#     a = list(map(int, input().split())) 
+    
+#     mp = {} 
+    
+#     for i in a: 
+#         mp[i] = 1 + mp.get(i, 0) 
+    
+#     dist, dup = len(mp), n - len(mp)
+    
+   
+    
+#     if dup >= k: 
+#         dup -= k 
+#     else: 
+#         dup = 0 
+    
+#     print(dist, dup)
+#     print(math.comb(n,2) - dup)
+        
+# for _ in range(int(input())): 
+#     solve()
 
-def main(): 
-    tests = int(input())
-    for _ in range(tests):
-        n = int(input())
-        yn = list(map(int, input().split()))  
-        memo = {}
-        print(helper(0, n, yn, memo))
+def solve(n, m, r, blue_balls):
+    red = {}
+    for xi, ui in r:
+        prod = xi * ui
+        if prod in red:
+            red[prod] += 1
+        else:
+            red[prod] = 1
 
-if __name__ == "__main__":
-    main()
+    ans = 0
+    for yi, vi in blue_balls:
+        prod = yi * vi
+        if prod in red and r[prod] > 0:
+            ans += 1
+            r[prod] -= 1
+
+    return ans
+
+n, m = map(int, input().split())
+r = [tuple(map(int, input().split())) for _ in range(n)]
+blue_balls = [tuple(map(int, input().split())) for _ in range(m)]
+
+print(solve(n, m, r, blue_balls))
