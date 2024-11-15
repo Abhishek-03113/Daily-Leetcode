430a8923fc3b8597f6f5feb44e0169690253ccdc
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2024-11-15 17:37:45 +0530

    update

--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,33 +1,7 @@
 import math
 from collections import Counter
 
-# def solve():
-    
-#     n, k = map(int, input().split()) 
-    
-#     a = list(map(int, input().split())) 
-    
-#     mp = {} 
-    
-#     for i in a: 
-#         mp[i] = 1 + mp.get(i, 0) 
-    
-#     dist, dup = len(mp), n - len(mp)
-    
-   
-    
-#     if dup >= k: 
-#         dup -= k 
-#     else: 
-#         dup = 0 
-    
-#     print(dist, dup)
-#     print(math.comb(n,2) - dup)
-        
-# for _ in range(int(input())): 
-#     solve()
-
-def solve(n, m, r, blue_balls):
+def solve(n, m, r, b):
     red = {}
     for xi, ui in r:
         prod = xi * ui
@@ -37,16 +11,16 @@ def solve(n, m, r, blue_balls):
             red[prod] = 1
 
     ans = 0
-    for yi, vi in blue_balls:
+    for yi, vi in b:
         prod = yi * vi
-        if prod in red and r[prod] > 0:
+        if prod in red and red[prod] > 0:
             ans += 1
-            r[prod] -= 1
+            red[prod] -= 1  
 
     return ans
 
 n, m = map(int, input().split())
 r = [tuple(map(int, input().split())) for _ in range(n)]
-blue_balls = [tuple(map(int, input().split())) for _ in range(m)]
+b = [tuple(map(int, input().split())) for _ in range(m)]
 
-print(solve(n, m, r, blue_balls))
+print(solve(n, m, r, b))
