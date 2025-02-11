88d12da26b6cd635947160d08724b2aa0bdf876c
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-02-11 19:37:39 +0530

    update

--- code/python/a.py
+++ code/python/a.py
@@ -1,16 +1,67 @@
-def closest_power_of_2_less_than_n(n):
-    if n <= 1:
-        return 0 
-    power = 1
-    while power * 2 <= n:
-        power *= 2
-    return power
-t = int(input()) 
+import sys
 
-for i in range(t):
-    n = int(input()) 
-    if n == 1:
-        print(1)
-    else:
-        print(closest_power_of_2_less_than_n(n))
-    
+def process_test_case(N, P, A):
+    left_max = [float('-inf')] * N
+    nearest_left = -1
+    current_max = 0
+    for i in range(N):
+        if A[i] == 0:
+            nearest_left = i
+            current_max = 0
+            left_max[i] = 0
+        else:
+            if nearest_left != -1:
+                current_max = max(current_max, A[i])
+                left_max[i] = current_max
+
+    right_max = [float('-inf')] * N
+    nearest_right = -1
+    current_max = 0
+    for i in range(N-1, -1, -1):
+        if A[i] == 0:
+            nearest_right = i
+            current_max = 0
+            right_max[i] = 0
+        else:
+            if nearest_right != -1:
+                current_max = max(current_max, A[i])
+                right_max[i] = current_max
+
+    result = []
+    for i in range(N):
+        if A[i] == 0:
+            result.append(0)
+        else:
+            x_left = None
+            if left_max[i] != float('-inf'):
+                x_left = max(A[i], left_max[i])
+            x_right = None
+            if right_max[i] != float('-inf'):
+                x_right = max(A[i], right_max[i])
+            
+            X = float('inf')
+            if x_left is not None:
+                X = x_left
+            if x_right is not None:
+                if x_right < X:
+                    X = x_right
+            
+            t = (X + P - 1) // P
+            result.append(t)
+    return result
+
+def main():
+    input = sys.stdin.read().split()
+    ptr = 0
+    T = int(input[ptr])
+    ptr += 1
+    for _ in range(T):
+        N, P = int(input[ptr]), int(input[ptr+1])
+        ptr += 2
+        A = list(map(int, input[ptr:ptr+N]))
+        ptr += N
+        res = process_test_case(N, P, A)
+        print(' '.join(map(str, res)))
+
+if __name__ == "__main__":
+    main()
--- code/python/codeforces.py
+++ code/python/codeforces.py
@@ -1,8 +1,41 @@
-n = int(input()) 
+def solve_test_case(N, K, A):
 
-a = list(map(int, input().split())) 
+    freq = {}
+    k_positions = {}
+
+    for i, num in enumerate(A):
+        freq[num] = freq.get(num, 0) + 1
+        if num == K:
+            k_positions[i] = True 
+
+    k_freq = freq.get(K, 0)
+    mx = max((freq[x] for x in freq if x != K), default=0)
+    
+    if len(A) > 2:
+        if k_freq > mx:
+            return 0
+
+        if k_positions.get(0, -1) != -1 or k_positions.get(N-1, -1) != -1:
+            return 1 
+
+        elif k_positions.get(1, -1) != -1 or k_positions.get(N-2, -1) != -1: 
+            return 1 
+        
+        else: 
+            return 2  
+    else: 
+        return 0 
+    
+    
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        N, K = map(int, input().split())
+        A = list(map(int, input().split()))
+        result = solve_test_case(N, K, A)
+        print(result)
 
 
-max_ = 0 
-ans = 0 
-
+if __name__ == "__main__":
+    main()
