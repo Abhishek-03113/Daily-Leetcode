cf594a8058590769149236bdd5a21fc2fbf888f3
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-03-30 17:52:54 +0530

    update

--- code/python/a.py
+++ code/python/a.py
@@ -1,20 +1,34 @@
-for _  in range(int(input())): 
-    
-    x,y,a = map(int,input().split()) 
-    a = a + 0.5
-    depth = 0 
-    turn = 0
-    while depth < a: 
-        
-        if turn == 0: 
-            depth += x
-            if depth >= a: 
-                print('NO')
-                break 
-        else: 
-            depth += y
-            if depth >= a: 
-                print('YES')
-                break
-        turn = 1 - turn
-    
+from collections import Counter
+
+
+def getResources(inc, dec, n, performance):
+    num_servers = len(performance)
+    resources = [0] * num_servers
+
+    ranks = []
+    for i in range(num_servers):
+
+        unique_values = set()
+        for p in performance:
+            if p >= performance[i]:
+                unique_values.add(p)
+        ranks.append(len(unique_values))
+
+    rank_counts = {}
+    for rank in ranks:
+        if rank in rank_counts:
+            rank_counts[rank] += 1
+        else:
+            rank_counts[rank] = 1
+
+    for i in range(num_servers):
+        resources[i] = inc * (n + 1 - ranks[i]) - dec * rank_counts[ranks[i]]
+
+    return resources
+
+
+inc = 200
+dec = 50
+n = 5
+performance = [2, 1, 3 ,3 ,1] 
+print(getResources(inc, dec, n, performance))
---
+++ code/python/dp.py
@@ -0,0 +1,12 @@
+# climbing stairs
+def help(n):
+
+    if n <= 1:
+        return n
+
+
+def main():
+
+    n = int(input())
+    
+    ans = help(n)
